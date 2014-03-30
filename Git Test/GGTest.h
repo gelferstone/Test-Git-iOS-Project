2b2ead14e2030f1bf5123b8cd0a9580373966fb2
gelferstone <ideainnovation@hotmail.com>
2014-03-29 22:54:36 -0400

    added a test class

---
+++ Git Test/GGTest.h
@@ -0,0 +1,13 @@
+//
+//  GGTest.h
+//  Git Test
+//
+//  Created by Pisit Luanprawat on 3/29/14.
+//  Copyright (c) 2014 Idea Innovation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GGTest : NSObject
+
+@end
---
+++ Git Test/GGTest.m
@@ -0,0 +1,13 @@
+//
+//  GGTest.m
+//  Git Test
+//
+//  Created by Pisit Luanprawat on 3/29/14.
+//  Copyright (c) 2014 Idea Innovation. All rights reserved.
+//
+
+#import "GGTest.h"
+
+@implementation GGTest
+
+@end
